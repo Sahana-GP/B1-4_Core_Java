f1bec4180b961576880a3abbea0b802730f484f7
SAHANA P <74894872+SAHANAN-P@users.noreply.github.com>
2022-04-01 19:46:34 +0530

    Update Constructor.java

--- Day3/src/day3/Constructor.java
+++ Day3/src/day3/Constructor.java
@@ -16,8 +16,8 @@ public class Constructor {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Constructor c=new Constructor("Soujan",55);
-		Constructor c1=new Constructor("Nikitha",25);
+		Constructor c=new Constructor("Sahana",50);
+		Constructor c1=new Constructor("Pooja",25);
 		c.display();
 		c1.display();
 	}
