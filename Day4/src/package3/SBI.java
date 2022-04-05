ea67fe34902dbf1e407b355e4d6976ff973d8601
SAHANA-P <sahanan1999gowda@gmail.com>
2022-04-05 13:46:54 +0530

    Day4

---
+++ Day4/src/package3/SBI.java
@@ -0,0 +1,17 @@
+package package3;
+
+public class SBI 
+{
+	public int balance;
+	public SBI(int bal)
+	{
+		balance=bal;
+	}
+	
+	public void display()
+	{
+		System.out.println(balance);
+	}
+	
+
+}
---
+++ Day4/src/package4/Encapsulation.java
@@ -0,0 +1,13 @@
+package package4;
+import package3.SBI;
+public class Encapsulation {
+
+	public static void main(String[] args)
+	{
+		SBI sb=new SBI(25000);
+		sb.display();
+
+
+	}
+
+}
